1403d68d7d3e26aa53286ae3078c8bb034c2a1e9
Dinesh Lodhavi <dineshlodhavi5@gmail.com>
2025-06-22 02:25:07 +0530

    Refactor Rock, Paper, Scissors game for improved structure and readability

--- beginner_project/Rock, Paper, Scissors game/rock_paper_scissors_game.py
+++ beginner_project/Rock, Paper, Scissors game/rock_paper_scissors_game.py
@@ -1,25 +1,40 @@
 import random
 
-while True:
-    choices = ('r','p','s')
-    emojis = {'r':'🪨','p':'📃','s':'✂️'}
-    user_choice = input("Rock, Paper, Scissors? (r/p/s): ").lower()
-    if user_choice not in choices:
-        print("Invalid choice!")
-        continue
+ROCK = 'r'
+SCISSORS = 's'
+PAPER = 'p'
+emojis = {ROCK:'🪨',PAPER:'📃',SCISSORS:'✂️'}
+choices = tuple(emojis.keys())
 
-    computer_choice = random.choice(choices)
+def get_user_choice():
+    while True:
+        user_choice =input("Rock, Paper, Scissors? (r/p/s): ").lower()
+        if user_choice not in choices:
+            print("Invalid choice!")
+        else:
+            return user_choice
+def display_choices(user_choice,computer_choice):
     print(f"Your choice is {emojis[user_choice]}")
     print(f"Computer choice is {emojis[computer_choice]}")
+def determine_winner(user_choice,computer_choice):
     if (
-        (user_choice == 'r' and computer_choice == 's') or
-        (user_choice == 's' and computer_choice == 'p') or
-        (user_choice == 'p' and computer_choice == 'r')
+        (user_choice == ROCK and computer_choice == SCISSORS) or
+        (user_choice == SCISSORS and computer_choice == PAPER) or
+        (user_choice == PAPER and computer_choice == ROCK)
     ):
         print("You win")
     else:
         print("You lose")
 
-    should_play = input("Continue(y/n)")
-    if should_play == 'n':
-        break
+def play_game():
+    while True:
+        user_choice = get_user_choice()
+
+        computer_choice = random.choice(choices)
+        display_choices(user_choice,computer_choice)
+        determine_winner(user_choice,computer_choice)
+
+        should_play = input("Continue(y/n)")
+        if should_play == 'n':
+            break
+play_game()
